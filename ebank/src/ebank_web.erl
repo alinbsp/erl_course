9e68d71aedd73c4ae31d1d86f4a188f66fd32afb
Georgiana Chelu <georgiana.chelu93@gmail.com>
2018-12-13 20:07:30 +0200

    Handle GET and POST in different functions

--- ebank/src/ebank_web.erl
+++ ebank/src/ebank_web.erl
@@ -25,87 +25,91 @@ start(Options) ->
 stop() ->
     mochiweb_http:stop(?MODULE).
 
+handle_get("hello_world", Req, _) ->
+   Req:respond({200, [{"Content-Type", "text/plain"}], "Hello world!\n"});
+
+handle_get("getBalance", Req, _) ->
+   QueryData = Req:parse_qs(),
+   QueryKeys = lists:sort(proplists:get_keys(QueryData)),
+   [Name, Pin] = lists:map(fun(X) -> proplists:get_value(X, QueryData) end, QueryKeys),
+   Accounts = mochiglobal:get(accounts),
+   case Accounts of
+      undefined ->
+         Body = "\n\tList of accounts is empty!";
+      _ ->
+         ListofAccountsrecord = convert_tuples_to_records(Accounts),
+         SearchAccountforBalance = get_balance(Name, Pin, ListofAccountsrecord),
+         case length(SearchAccountforBalance) of
+            0 ->
+               Body = "\n\tAccount not found!";
+            _ ->
+               Body = generate_body(hd(SearchAccountforBalance))
+            end
+      end,
+   Req:respond({200, [{"Content-Type", "text/plain"}], Body});
+
+handle_get("check", Req, _) ->
+   QueryData = Req:parse_qs(),
+   QueryKeys = proplists:get_keys(QueryData),
+   Id = lists:map(fun(X) -> proplists:get_value(X, QueryData) end, QueryKeys),
+   Accounts = mochiglobal:get(accounts),
+   case Accounts of
+      undefined ->
+         Body = "\n\tList of accounts is empty!";
+      _ ->
+         ListofAccountsrecord = convert_tuples_to_records(Accounts),
+         IsAccount = lists:map(fun(X) ->  io:format("Account record format: ~p", [X#account.details#accountDetails.name])end, ListofAccountsrecord),
+         Account = search_account_byId(hd(Id), ListofAccountsrecord),
+         case length(Account) of
+            0 ->
+               Body =  "\n\tAccount not found!";
+            _ ->
+               Body = generate_body(hd(Account))
+         end
+   end,
+   Req:respond({200, [{"Content-Type", "text/plain"}], Body});
+
+handle_get(Path, Req, DocRoot) ->
+   Req:serve_file(Path, DocRoot).
+
+handle_post("create", Req, _) ->
+   QueryData = Req:parse_qs(),
+   %[{id, Id}, {name, Name}, {pin, Pin}, {balance, Balance}] = QueryData,
+   QueryKeys = lists:sort(proplists:get_keys(QueryData)),
+   [Balance, Id, Name, Pin] = lists:map(fun(X) -> proplists:get_value(X, QueryData) end, QueryKeys),
+   Adet = #accountDetails{name=Name, balance=Balance, pin=Pin},
+   Account = #account{id=Id, details=Adet},
+   Accounts = mochiglobal:get(accounts),
+   case Accounts of
+      undefined  ->
+         NewAccounts = [Account],
+         mochiglobal:put(accounts, NewAccounts),
+         Body = generate_body(Account);
+      _ ->
+         SearchAccount = search_account_byId(Id, Accounts),
+
+         case length(SearchAccount) of
+            0 ->
+               NewAccounts = [Account|Accounts],
+               mochiglobal:put(accounts, NewAccounts),
+               Body = generate_body(Account);
+            _ ->
+               Body = "\n\t An account with this id already exists!"
+         end
+   end,
+   Req:respond({200, [{"Content-Type", "text/plain"}], Body});
+
+handle_post(_Path, Req, _DocRoot) ->
+   Req:not_found().
+
 loop(Req, DocRoot) ->
     "/" ++ Path = Req:get(path),
     try
       case Req:get(method) of
          Method when Method =:= 'GET'; Method =:= 'HEAD' ->
-         case Path of
-            "hello_world" ->
-               Req:respond({200, [{"Content-Type", "text/plain"}], "Hello world!\n"});
-            "getBalance" ->
-               QueryData = Req:parse_qs(),
-               QueryKeys = lists:sort(proplists:get_keys(QueryData)),
-               [Name, Pin] = lists:map(fun(X) -> proplists:get_value(X, QueryData) end, QueryKeys),
-	            Accounts = mochiglobal:get(accounts),
-               case Accounts of
-                  undefined ->
-                     Body = "\n\tList of accounts is empty!";
-                  _ ->
-                     ListofAccountsrecord = convert_tuples_to_records(Accounts),
-                     SearchAccountforBalance = get_balance(Name, Pin, ListofAccountsrecord),
-                     case length(SearchAccountforBalance) of
-                        0 -> 
-                           Body = "\n\tAccount not found!";
-                        _ ->
-                           Body = generate_body(hd(SearchAccountforBalance))
-                        end
-                  end,
-               Req:respond({200, [{"Content-Type", "text/plain"}], Body});
-            "check" ->
-               QueryData = Req:parse_qs(),
-               QueryKeys = proplists:get_keys(QueryData),
-               Id = lists:map(fun(X) -> proplists:get_value(X, QueryData) end, QueryKeys),
-               Accounts = mochiglobal:get(accounts),
-               case Accounts of 
-                  undefined ->
-                     Body = "\n\tList of accounts is empty!";
-                  _ ->
-                     ListofAccountsrecord = convert_tuples_to_records(Accounts),
-                     IsAccount = lists:map(fun(X) ->  io:format("Account record format: ~p", [X#account.details#accountDetails.name])end, ListofAccountsrecord),
-                     Account = search_account_byId(hd(Id), ListofAccountsrecord),
-                     case length(Account) of 
-                        0 ->
-                           Body =  "\n\tAccount not found!";
-                        _ -> 
-                           Body = generate_body(hd(Account))
-                     end
-               end,
-               Req:respond({200, [{"Content-Type", "text/plain"}], Body});
-            _ ->
-               Req:serve_file(Path, DocRoot)
-            end;
+            handle_get(Path, Req, DocRoot);
          'POST' ->
-         case Path of
-            "create" ->
-		   	   QueryData = Req:parse_qs(),
-			      %[{id, Id}, {name, Name}, {pin, Pin}, {balance, Balance}] = QueryData,
-			      QueryKeys = lists:sort(proplists:get_keys(QueryData)),
-      			[Balance, Id, Name, Pin] = lists:map(fun(X) -> proplists:get_value(X, QueryData) end, QueryKeys),
-      	   	Adet = #accountDetails{name=Name, balance=Balance, pin=Pin},
-               Account = #account{id=Id, details=Adet},
-			      Accounts = mochiglobal:get(accounts),
-               case Accounts of
-                  undefined  -> 
-                     NewAccounts = [Account],
-                     mochiglobal:put(accounts, NewAccounts),
-                     Body = generate_body(Account);
-                  _ -> 
-                     SearchAccount = search_account_byId(Id, Accounts),
-                     
-                     case length(SearchAccount) of
-                        0 ->
-                           NewAccounts = [Account|Accounts],
-			                  mochiglobal:put(accounts, NewAccounts),
-                           Body = generate_body(Account);
-                        _ ->
-                           Body = "\n\t An account with this id already exists!"
-                     end
-               end,
-               Req:respond({200, [{"Content-Type", "text/plain"}], Body});
-            _ ->
-               Req:not_found()
-         end;
+            handle_post(Path, Req, DocRoot);
          _ ->
             Req:respond({501, [], []})
         end
@@ -117,24 +121,24 @@ loop(Req, DocRoot) ->
                   {trace, erlang:get_stacktrace()}],
       error_logger:error_report(Report),
       Req:respond({500, [{"Content-Type", "text/plain"}], "request failed, sorry\n"})
-   end. 
+   end.
 
 %% Internal API
 get_option(Option, Options) ->
    {proplists:get_value(Option, Options), proplists:delete(Option, Options)}.
 
-generate_body(#account{id=Id, details=#accountDetails{name = Name, balance = Balance,  pin = Pin}}) ->
-   Body = "{\n\t\"Name\":\"" ++ Name ++ "\",\n" ++  "\t\"Balance\":\"" ++ Balance ++ "\"\n}".
+generate_body(#account{id = _Id, details=#accountDetails{name = Name, balance = Balance,  pin = _Pin}}) ->
+   "{\n\t\"Name\":\"" ++ Name ++ "\",\n" ++  "\t\"Balance\":\"" ++ Balance ++ "\"\n}".
 
-search_account_byId(Id, Accounts) -> 
-   SearchAccount = lists:filter(fun(X) -> X#account.id =:= Id end, Accounts).
+search_account_byId(Id, Accounts) ->
+   lists:filter(fun(X) -> X#account.id =:= Id end, Accounts).
 
 convert_tuples_to_records(Accounts) ->
-   Listofaccounts = lists:map(fun(X) -> #account{id = element(2,X), details = 
+   lists:map(fun(X) -> #account{id = element(2,X), details =
       #accountDetails{name=element(2,element(3,X)), balance =element(3,element(3,X)), pin = element(4,element(3,X))}} end, Accounts).
 
 get_balance(Name, Pin, Accounts) ->
-   Account = lists:filter(fun(X) -> X#account.details#accountDetails.name =:= Name andalso X#account.details#accountDetails.pin =:= Pin end, Accounts).
+   lists:filter(fun(X) -> X#account.details#accountDetails.name =:= Name andalso X#account.details#accountDetails.pin =:= Pin end, Accounts).
 
 %%
 %% Tests
