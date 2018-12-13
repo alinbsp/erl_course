59149765727b6c5344dc06cc65227c91d0264f66
diana.dimitriu@endava.com <diana.dimitriu@endava.com>
2018-12-13 05:35:32 -0500

    Get balance function & endpoint + Convert tuples to records function + 2 tests for the new functions

--- ebank/src/ebank_web.erl
+++ ebank/src/ebank_web.erl
@@ -34,26 +34,41 @@ loop(Req, DocRoot) ->
             "hello_world" ->
                Req:respond({200, [{"Content-Type", "text/plain"}], "Hello world!\n"});
             "getBalance" ->
+               QueryData = Req:parse_qs(),
+               QueryKeys = lists:sort(proplists:get_keys(QueryData)),
+               [Name, Pin] = lists:map(fun(X) -> proplists:get_value(X, QueryData) end, QueryKeys),
 	            Accounts = mochiglobal:get(accounts),
-               Req:respond({200, [{"Content-Type", "text/plain"}], "{\"Balance\": 100}\n"});
+               case Accounts of
+                  undefined ->
+                     Body = "\n\tList of accounts is empty!";
+                  _ ->
+                     ListofAccountsrecord = convert_tuples_to_records(Accounts),
+                     SearchAccountforBalance = get_balance(Name, Pin, ListofAccountsrecord),
+                     case length(SearchAccountforBalance) of
+                        0 -> 
+                           Body = "\n\tAccount not found!";
+                        _ ->
+                           Body = generate_body(hd(SearchAccountforBalance))
+                        end
+                  end,
+               Req:respond({200, [{"Content-Type", "text/plain"}], Body});
             "check" ->
                QueryData = Req:parse_qs(),
                QueryKeys = proplists:get_keys(QueryData),
                Id = lists:map(fun(X) -> proplists:get_value(X, QueryData) end, QueryKeys),
-               Idint = list_to_integer(hd(Id)),
                Accounts = mochiglobal:get(accounts),
                case Accounts of 
                   undefined ->
                      Body = "\n\tList of accounts is empty!";
                   _ ->
-                     Account = lists:filter(fun(X) -> list_to_integer(element(2, X)) =:= Idint end,  Accounts),
+                     ListofAccountsrecord = convert_tuples_to_records(Accounts),
+                     IsAccount = lists:map(fun(X) ->  io:format("Account record format: ~p", [X#account.details#accountDetails.name])end, ListofAccountsrecord),
+                     Account = search_account_byId(hd(Id), ListofAccountsrecord),
                      case length(Account) of 
                         0 ->
                            Body =  "\n\tAccount not found!";
                         _ -> 
-                           io:format("Account: ~p", [Account]),  
-                           Body = "{\n\t\"Name\":" ++ io_lib:format("~p", [element(2, element(3,(hd(Account))))]) ++ ",\n" ++  "\t\"Balance\":" 
-                                                   ++ io_lib:format("~p", [element(3, element(3,(hd(Account))))]) ++ "\n}"
+                           Body = generate_body(hd(Account))
                      end
                end,
                Req:respond({200, [{"Content-Type", "text/plain"}], Body});
@@ -74,17 +89,14 @@ loop(Req, DocRoot) ->
                   undefined  -> 
                      NewAccounts = [Account],
                      mochiglobal:put(accounts, NewAccounts),
-                     % Body = "{\n\t\"Name\":\"" ++ Adet#accountDetails.name ++ "\",\n" ++  "\t\"Balance\":\"" ++ Adet#accountDetails.balance ++ "\"\n}";
                      Body = generate_body(Account);
                   _ -> 
-                     % SearchAccount = lists:filter(fun(X) -> X#account.id =:= Id end,  Accounts),
                      SearchAccount = search_account_byId(Id, Accounts),
                      
                      case length(SearchAccount) of
                         0 ->
                            NewAccounts = [Account|Accounts],
 			                  mochiglobal:put(accounts, NewAccounts),
-                           % Body = "{\n\t\"Name\":\"" ++ Adet#accountDetails.name ++ "\",\n" ++  "\t\"Balance\":\"" ++ Adet#accountDetails.balance ++ "\"\n}";
                            Body = generate_body(Account);
                         _ ->
                            Body = "\n\t An account with this id already exists!"
@@ -117,6 +129,13 @@ generate_body(#account{id=Id, details=#accountDetails{name = Name, balance = Bal
 search_account_byId(Id, Accounts) -> 
    SearchAccount = lists:filter(fun(X) -> X#account.id =:= Id end, Accounts).
 
+convert_tuples_to_records(Accounts) ->
+   Listofaccounts = lists:map(fun(X) -> #account{id = element(2,X), details = 
+      #accountDetails{name=element(2,element(3,X)), balance =element(3,element(3,X)), pin = element(4,element(3,X))}} end, Accounts).
+
+get_balance(Name, Pin, Accounts) ->
+   Account = lists:filter(fun(X) -> X#account.details#accountDetails.name =:= Name andalso X#account.details#accountDetails.pin =:= Pin end, Accounts).
+
 %%
 %% Tests
 %%
@@ -134,5 +153,22 @@ Account2 = #account{id = 2, details=#accountDetails{name="account2", balance="20
 Accounts = [Account1, Account2],
 Id = 1,
 ?_assertEqual([Account1], search_account_byId(Id, Accounts)).
+
+convert_tuples_to_records_test_() ->
+Account1 = #account{id = 1, details=#accountDetails{name="account1", balance="1000", pin=12345}},
+Account2 = #account{id = 2, details=#accountDetails{name="account2", balance="200", pin=12}},
+Accounts = [Account1, Account2],
+Accounttuple1 = {"account", 1, {"accountDetails", "account1", "1000", 12345}},
+Accounttuple2 = {"account", 2, {"accountDetails", "account2", "200", 12}},
+Accountstuples = [Accounttuple1, Accounttuple2],
+?_assertEqual(Accounts, convert_tuples_to_records(Accountstuples)).
+
+get_balance_test_() ->
+Account1 = #account{id = 1, details=#accountDetails{name="account1", balance="1000", pin=12345}},
+Account2 = #account{id = 2, details=#accountDetails{name="account2", balance="200", pin=12}},
+Accounts = [Account1, Account2],
+Name = "account1",
+Pin = 12345,
+?_assertEqual([Account1], get_balance(Name, Pin, Accounts)).
 
 -endif.
